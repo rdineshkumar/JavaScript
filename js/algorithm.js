8eabfd9b0d033a626b7f4646a4fd700d4e6899d1
harism <harism@gmail.com>
2012-10-12 01:16:00 +0300

    Algorithm optimized further.

--- js/algorithm.js
+++ js/algorithm.js
@@ -1,10 +1,9 @@
-
 define(function() {
 
 	// Constant employees JSON file path.
 	var JSONPath = "data/employees.json";
 	// Constant friend id.
-	var friendId = 1009;
+	var friendId = "1009";
 
 	// Hook into run button.
 	document.getElementById("run").onclick = function() {
@@ -24,26 +23,38 @@ define(function() {
 		employeesJSON = employeesJSON.replace(/\"\s*\]\s*$/, "");
 		// Split pairs from remaining string using '" , "' as separator.
 		var pairs = employeesJSON.split(/\"\s*,\s*\"/);
+		// Split pairs into 3d array.
+		for ( var i = 0; i < pairs.length; ++i) {
+			pairs[i] = pairs[i].split(" ");
+		}
+		
+		pairs = [];
+		for (var i=0; i<1000; ++i) {
+			pairs.push([]);
+			pairs[i].push((Math.floor(Math.random() * 999 + 1000)) + "");
+			pairs[i].push((Math.floor(Math.random() * 999 + 2000)) + "");
+		}
 
 		// First create an array where your friend is chosen to Barbados and
 		// calculate how many attendees it would take.
-		var pairsWithFriend = [].concat(pairs);
-		choose(pairsWithFriend, friendId);
-		findLuckyOnes(pairsWithFriend);
+		var pairsWithFriend = pairs.slice(0, pairs.length);
+		remove(pairsWithFriend, friendId);
+		var luckyOnesWithFriend = findLuckyOnes(pairsWithFriend);
+		luckyOnesWithFriend.push(friendId);
 
 		// Then calculate minimum travelers value from original array.
-		findLuckyOnes(pairs);
+		var luckyOnes = findLuckyOnes(pairs);
 
 		// If array containing your friend is shorter or same length as original
 		// array minimum, choose it as output.
-		if (pairsWithFriend.length <= pairs.length) {
-			pairs = pairsWithFriend;
+		if (luckyOnesWithFriend.length <= luckyOnes.length) {
+			luckyOnes = luckyOnesWithFriend;
 		}
 
 		// Output selected pairs into selectees list.
 		var outputList = "";
-		for ( var i = 0; i < pairs.length; ++i) {
-			outputList += "<li>" + pairs[i] + "</li>";
+		for ( var i = 0; i < luckyOnes.length; ++i) {
+			outputList += "<li>" + luckyOnes[i] + "</li>";
 		}
 		document.getElementById("selectees").innerHTML = outputList;
 	}
@@ -53,6 +64,10 @@ define(function() {
 		// 1. Find ids with highest occurrence count.
 		// 2. Choose random id from highest count id array and replace all
 		// pairs containing that id with it only.
+
+		// Array for constructing lucky ones ids.
+		var luckyOnes = [];
+
 		do {
 			// Maximum count variable.
 			var maxCount = 0;
@@ -60,46 +75,58 @@ define(function() {
 			var maxIds = [];
 
 			// Iterate over all pairs to find maxCount value and corresponding
-			// array
-			// of ids.
+			// array of ids.
 			for ( var i = 0; i < pairs.length; ++i) {
-				// First check that pairs item contains pair of ids. Otherwise
-				// it
-				// has been replaced with one id only (going to Barbados) and
-				// ought
-				// to be skipped.
-				var ids = pairs[i].split(" ");
-				if (ids.length != 2) {
-					continue;
-				}
 				// Check both ids whether they have higher or equal occurrences
 				// compared to maxCount.
-				for ( var j = 0; j < ids.length; ++j) {
+				for ( var j = 0; j < pairs[i].length; ++j) {
+					if (maxIds.indexOf(pairs[i][j]) != -1) {
+						continue;
+					}
+
 					// Number of occurrences an id is contained in pairs array.
-					var count = countIds(pairs, ids[j]);
+					var count = countIds(pairs, pairs[i][j]);
 					// If count is at least maxCount.
 					if (count >= maxCount) {
 						// If count is higher than previous maxCount, clear
-						// maxIds
-						// array.
+						// maxIds array.
 						if (count > maxCount) {
 							maxIds.splice(0, maxIds.length);
 						}
 						// Add id to maxIds array.
-						maxIds.splice(0, 0, ids[j]);
+						maxIds.push(pairs[i][j]);
 						maxCount = count;
 					}
 				}
 			}
 
 			// If maxCount greater than zero, choose random id from maximum ids
-			// array to Barbados.
+			// array to Barbados travelers.
 			if (maxCount > 0) {
-				choose(pairs, maxIds[Math.floor(Math.random() * maxIds.length)]);
+				// Iterate over all maxIds plus remove ones from it whose count
+				// changes during removal operations.
+				while (maxIds.length > 0) {
+					// Choose random index.
+					var randIdx = Math.floor(Math.random() * maxIds.length);
+					// Push randIdx id to lucky ones going to Barbados.
+					luckyOnes.push(maxIds[randIdx]);
+					// Remove all pairs containing randIdx id and remove ids
+					// from maxIds whose count changed during removal operation.
+					var removedIds = remove(pairs, maxIds[randIdx]);
+					for ( var i = 0; i < removedIds.length; ++i) {
+						var idx = maxIds.indexOf(removedIds[i]);
+						if (idx != -1) {
+							maxIds.splice(idx, 1);
+						}
+					}
+				}
 			}
 		}
 		// Continue while maxCount is higher than zero.
 		while (maxCount > 0);
+
+		// Return array of lucky ones ids.
+		return luckyOnes;
 	}
 
 	//
@@ -110,45 +137,30 @@ define(function() {
 	function countIds(pairs, id) {
 		var count = 0;
 		for ( var i = 0; i < pairs.length; ++i) {
-			var ids = pairs[i].split(" ");
-			if (ids.length == 2) {
-				if (ids[0] == id) {
-					++count;
-				}
-				if (ids[1] == id) {
-					++count;
-				}
+			if (pairs[i].indexOf(id) != -1) {
+				++count;
 			}
 		}
 		return count;
 	}
 
 	//
-	// Replaces a pair that contains given id with id only.
+	// Remove pairs that contain given id.
 	//
-	function choose(pairs, id) {
-		// First occurrence is replaced, latter ones are removed.
-		var replace = true;
-
+	function remove(pairs, id) {
+		var removedIds = [ id ];
 		// Iterate through all pairs.
 		for ( var i = 0; i < pairs.length;) {
-			// Find ids from pair.
-			var ids = pairs[i].split(" ");
-			// If there are two ids and either one equals id..
-			if (ids.length == 2 && (ids[0] == id || ids[1] == id)) {
-				// Replace pair with id string.
-				if (replace) {
-					pairs.splice(i++, 1, id + "");
-					replace = false;
-				}
-				// Otherwise remove pair.
-				else {
-					pairs.splice(i, 1);
-				}
+			// If either one of the ids equals id..
+			var idx = pairs[i].indexOf(id);
+			if (idx != -1) {
+				removedIds.push(pairs[i][1 - idx]);
+				pairs.splice(i, 1);
 			} else {
 				++i;
 			}
 		}
+		return removedIds;
 	}
 
 	//
