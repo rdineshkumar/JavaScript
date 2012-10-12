1a2e7deefa3b68e29408423fd1679f78aaf0952f
harism <harism@gmail.com>
2012-10-12 04:26:06 +0300

    Algorithm optimized further plus more/less rewritten.

--- js/algorithm.js
+++ js/algorithm.js
@@ -23,27 +23,24 @@ define(function() {
 		employeesJSON = employeesJSON.replace(/\"\s*\]\s*$/, "");
 		// Split pairs from remaining string using '" , "' as separator.
 		var pairs = employeesJSON.split(/\"\s*,\s*\"/);
-		// Split pairs into 3d array.
+		// Generate dictionary into which we gather all pairs working together.
+		var countDict = {};
 		for ( var i = 0; i < pairs.length; ++i) {
-			pairs[i] = pairs[i].split(" ");
-		}
-		
-		pairs = [];
-		for (var i=0; i<1000; ++i) {
-			pairs.push([]);
-			pairs[i].push((Math.floor(Math.random() * 999 + 1000)) + "");
-			pairs[i].push((Math.floor(Math.random() * 999 + 2000)) + "");
+			var pair = pairs[i].split(" ");
+			for ( var j = 0; j < 2; ++j) {
+				if (countDict[pair[j]] == null) {
+					countDict[pair[j]] = [];
+				}
+				countDict[pair[j]].push(pair[1 - j]);
+			}
 		}
 
-		// First create an array where your friend is chosen to Barbados and
+		// First calculate minimum travelers value from original dictionary.
+		var luckyOnes = findLuckyOnes(countDict, []);
+
+		// Then create an array where your friend is chosen to Barbados and
 		// calculate how many attendees it would take.
-		var pairsWithFriend = pairs.slice(0, pairs.length);
-		remove(pairsWithFriend, friendId);
-		var luckyOnesWithFriend = findLuckyOnes(pairsWithFriend);
-		luckyOnesWithFriend.push(friendId);
-
-		// Then calculate minimum travelers value from original array.
-		var luckyOnes = findLuckyOnes(pairs);
+		var luckyOnesWithFriend = findLuckyOnes(countDict, [ friendId ]);
 
 		// If array containing your friend is shorter or same length as original
 		// array minimum, choose it as output.
@@ -59,108 +56,102 @@ define(function() {
 		document.getElementById("selectees").innerHTML = outputList;
 	}
 
-	function findLuckyOnes(pairs) {
+	//
+	// Choose employees to go to Barbados from pairs array.
+	//
+	function findLuckyOnes(dict, luckyOnes) {
+
+		// Generate array from dictionary. 'id' is employee id and 'partners' is
+		// an array of employees the person works with.
+		var countArray = [];
+		for (key in dict) {
+			countArray.push({
+				id : key,
+				partners : [].concat(dict[key])
+			});
+		}
+
+		// Remove already chosen employees from countArray.
+		for ( var i = 0; i < luckyOnes.length; ++i) {
+			removeAndDecrease(countArray, luckyOnes[i]);
+		}
+
 		// Algorithm works as follows;
-		// 1. Find ids with highest occurrence count.
-		// 2. Choose random id from highest count id array and replace all
-		// pairs containing that id with it only.
+		// 1. Sort array plus put employees with similarly many projects into
+		// random order.
+		// 2. Choose ids with highest count and remove them from countArray.
+		while (countArray.length > 0) {
+			// Sort and randomize countArray.
+			countArray.sort(function(a, b) {
+				if (b['partners'].length == a['partners'].length) {
+					return Math.random() >= 0.5 ? 1 : -1;
+				}
+				return b['partners'].length - a['partners'].length;
+			});
 
-		// Array for constructing lucky ones ids.
-		var luckyOnes = [];
-
-		do {
-			// Maximum count variable.
-			var maxCount = 0;
-			// Array of ids that have maxCount occurrences.
-			var maxIds = [];
-
-			// Iterate over all pairs to find maxCount value and corresponding
-			// array of ids.
-			for ( var i = 0; i < pairs.length; ++i) {
-				// Check both ids whether they have higher or equal occurrences
-				// compared to maxCount.
-				for ( var j = 0; j < pairs[i].length; ++j) {
-					if (maxIds.indexOf(pairs[i][j]) != -1) {
-						continue;
-					}
-
-					// Number of occurrences an id is contained in pairs array.
-					var count = countIds(pairs, pairs[i][j]);
-					// If count is at least maxCount.
-					if (count >= maxCount) {
-						// If count is higher than previous maxCount, clear
-						// maxIds array.
-						if (count > maxCount) {
-							maxIds.splice(0, maxIds.length);
-						}
-						// Add id to maxIds array.
-						maxIds.push(pairs[i][j]);
-						maxCount = count;
-					}
+			// maxLength is the the first item partners count.
+			var maxLength = countArray[0]['partners'].length;
+			// maxIndex is the highest index containing maxLength partners.
+			var maxIndex = 1;
+			while (maxIndex < countArray.length) {
+				if (countArray[maxIndex]['partners'].length == maxLength) {
+					++maxIndex;
+				} else {
+					break;
 				}
 			}
-
-			// If maxCount greater than zero, choose random id from maximum ids
-			// array to Barbados travelers.
-			if (maxCount > 0) {
-				// Iterate over all maxIds plus remove ones from it whose count
-				// changes during removal operations.
-				while (maxIds.length > 0) {
-					// Choose random index.
-					var randIdx = Math.floor(Math.random() * maxIds.length);
+			// Iterate over all indices until maxIndex.
+			for ( var i = 0; i < maxIndex && i < countArray.length;) {
+				var employee = countArray[i];
+				// Re-check partners length as it changes during the loop.
+				if (employee['partners'].length == maxLength) {
 					// Push randIdx id to lucky ones going to Barbados.
-					luckyOnes.push(maxIds[randIdx]);
-					// Remove all pairs containing randIdx id and remove ids
-					// from maxIds whose count changed during removal operation.
-					var removedIds = remove(pairs, maxIds[randIdx]);
-					for ( var i = 0; i < removedIds.length; ++i) {
-						var idx = maxIds.indexOf(removedIds[i]);
-						if (idx != -1) {
-							maxIds.splice(idx, 1);
-						}
-					}
+					luckyOnes.push(employee['id']);
+					// Remove employee with given id from countArray plus from
+					// other employee partners list.
+					removeAndDecrease(countArray, employee['id']);
+				} else {
+					++i;
 				}
 			}
 		}
-		// Continue while maxCount is higher than zero.
-		while (maxCount > 0);
 
 		// Return array of lucky ones ids.
 		return luckyOnes;
 	}
 
 	//
-	// Calculates number of times given id exists
-	// in id pairs array. Calculates only items that
-	// do have two ids.
+	// Removes employee with given id from employees list. Additionally removes
+	// it from other employees 'partner' lists too. And finally removes all
+	// employees whose partner count is zero.
 	//
-	function countIds(pairs, id) {
-		var count = 0;
-		for ( var i = 0; i < pairs.length; ++i) {
-			if (pairs[i].indexOf(id) != -1) {
-				++count;
+	function removeAndDecrease(employees, id) {
+		// Remove given id from employees list and from employees who got it as
+		// a partner.
+		for ( var i = 0; i < employees.length;) {
+			var employee = employees[i];
+			if (employee['id'] == id) {
+				employees.splice(i, 1);
+			} else {
+				for ( var j = 0; j < employee['partners'].length;) {
+					if (employee['partners'][j] == id) {
+						employee['partners'].splice(j, 1);
+					} else {
+						++j;
+					}
+				}
+				++i;
 			}
 		}
-		return count;
-	}
-
-	//
-	// Remove pairs that contain given id.
-	//
-	function remove(pairs, id) {
-		var removedIds = [ id ];
-		// Iterate through all pairs.
-		for ( var i = 0; i < pairs.length;) {
-			// If either one of the ids equals id..
-			var idx = pairs[i].indexOf(id);
-			if (idx != -1) {
-				removedIds.push(pairs[i][1 - idx]);
-				pairs.splice(i, 1);
+		// Remove employees whose partner count is zero.
+		for ( var i = 0; i < employees.length;) {
+			var employee = employees[i];
+			if (employee['partners'].length == 0) {
+				employees.splice(i, 1);
 			} else {
 				++i;
 			}
 		}
-		return removedIds;
 	}
 
 	//
